e632a98d5d7b89e364143959166416f5d82c75be
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-09-17 19:04:31 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -25,7 +25,7 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 if [ -z "$NEEDS_TAG" ]; then
     git tag $NEW_TAG
     echo "Tagged with $NEW_TAG"
-    git push https://nishanth.g@successive.tech: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@github.com/Nishanth80/react-native-starter.git
+    git push https://Nishanth80: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@github.com/Nishanth80/react-native-starter.git
 
      #git push origin tag $NEW_TAG
     
