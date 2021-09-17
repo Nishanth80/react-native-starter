22af8a8e1c02b4447f04fe2a6ae69c877d2cf329
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-09-17 16:25:42 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -25,9 +25,8 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 if [ -z "$NEEDS_TAG" ]; then
     git tag $NEW_TAG
     echo "Tagged with $NEW_TAG"
-    cd /var/lib/jenkins/workspace/Lakeshore-1
-    chmod 777 /var/lib/jenkins/workspace/Lakeshore-1
-    git push --tags git@github.com:Nishanth80/react-native-starter.git
+    
+    git push --tags Nishanth80:Nishanth@7256@git@github.com:Nishanth80/react-native-starter.git
 else
     echo "Already a tag on this commit"
 fi
