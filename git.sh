f2b6e8d478ee5eb767f12691e9cf478253d228f4
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-09-18 23:12:11 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -7,28 +7,24 @@ VERSION=`git describe --abbrev=0 --tags`
 VERSION_BITS=(${VERSION//./ })
 
 #get number parts and increase last one by 1
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
-VNUM3=$((VNUM3+1))
+vNUM1=${VERSION_BITS[0]}
+vNUM2=${VERSION_BITS[1]}
+vNUM3=${VERSION_BITS[2]}
+vNUM3=$((vNUM3+1))
 
 #create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+NEW_TAG="$vNUM1.$vNUM2.$vNUM3"
 
 echo "Updating $VERSION to $NEW_TAG"
 
-#get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+get current hash and see if it already has a tag
+$GIT_COMMIT=`git rev-parse HEAD`
+#NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
-#only tag if no tag already
-if [ -z "$NEEDS_TAG" ]; then
-    git tag $NEW_TAG
+only tag if no tag already
+#if [ -z "$NEEDS_TAG" ]; then
+    git tag -a $NEW_TAG -m $NEW_TAG
     echo "Tagged with $NEW_TAG"
-     
-     git push https://Nishanth80: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@github.com/Nishanth80/react-native-starter.git tag $NEW_TAG
-    
-    #git push --tags 
-else
-    echo "Already a tag on this commit"
-fi
+#else
+    #echo "Already a tag on this commit"
+#fi
