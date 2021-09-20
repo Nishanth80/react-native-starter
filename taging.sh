5676f3418ad3e14c1c41e1195673aa246e3387c4
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2021-09-20 12:19:51 +0530

    Update taging.sh

--- taging.sh
+++ taging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #get highest tag number
-VERSION=`git describe --abbrev=0 --tags`
+VERSION='git describe --abbrev=0 --tags'
 
 #replace . with space so can split into an array
 VERSION_BITS=(${VERSION//./ })
@@ -18,13 +18,13 @@ NEW_TAG="$vNUM1.$vNUM2.$vNUM3"
 echo "Updating $VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+GIT_COMMIT='git rev-parse HEAD'
+NEEDS_TAG='git describe --contains $GIT_COMMIT 2>/dev/null'
 
 only tag if no tag already
 #if [ -z "$NEEDS_TAG" ]; then
     git tag -a $NEW_TAG -m $NEW_TAG
     echo "Tagged with $NEW_TAG"
 #else
-    echo "Already a tag on this commit"
+    #echo "Already a tag on this commit"
 #fi
